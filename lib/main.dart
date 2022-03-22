b55a502c2f53e2baeab7f0d7cca0297df79a13ca
SorinnyDev <cjkklled@gmail.com>
2022-03-22 18:07:15 +0900

    바텀 네비게이션 추가 220322

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:simple_shadow/simple_shadow.dart';
+
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+import 'package:circular_bottom_navigation/tab_item.dart';
 
 import 'data/database.dart';
 import 'data/lotto.dart';
 import 'data/util.dart';
+
+import 'screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,34 +41,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final dbHelper = DatabaseHelper.instance;
 
-  List<List<int>> list_lotto = [];
+  //bottom navigation
+  int selectedPos = 2;
+  
+  double bottomNavBarHeight = 40;
 
-  void _addLotto() {
-    setState(() {
-      List<int> lotto = [];
-      while (true) {
-        int rnd = Random().nextInt(45) + 1;
-        if (!lotto.contains(rnd)) {
-          lotto.add(rnd);
-        }
-        if (lotto.length == 6) break;
-      }
+  List<TabItem> tabItems = List.of([
+    new TabItem(Icons.qr_code_scanner_rounded, "", Colors.blue, labelStyle: TextStyle(fontWeight: FontWeight.normal)),
+    new TabItem(Icons.search, "", Colors.orange, labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+    new TabItem(Icons.home, "", Color(0xff271f99),),
+    new TabItem(Icons.notifications, "", Colors.green),
+    new TabItem(Icons.more_horiz_rounded, "", Colors.cyan),
+  ]);
 
-      lotto.sort();
-      list_lotto.add(lotto);
-    });
-  }
+  late CircularBottomNavigationController _navigationController;
 
-  void _tableDrop() {
-    //드랍 테이블
-    dbHelper.dropTable('getchaLotto');
-    print("드랍 테이블");
-
-    super.setState(() {
-
-      list_lotto = [];
-
-    });
+  @override
+  void initState() {
+    super.initState();
+    _navigationController = new CircularBottomNavigationController(selectedPos);
   }
 
   @override
@@ -73,23 +67,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("로또 겟차"),
+        backgroundColor: Color(0xff271f99),
+        title: Text("로또 겟차", style: TextStyle(fontWeight: FontWeight.bold),),
       ),
-      body: ListView.separated(
-          padding: EdgeInsets.all(8),
-          reverse: false,
-          itemCount: list_lotto.length,
-          itemBuilder: (context, index) {
-            return getNumToImg2(list_lotto[index]);
-          },
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
+      body: Stack(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(bottom: bottomNavBarHeight),child: bodyContainer(),),
+          Align(alignment: Alignment.bottomCenter, child: bottomNav()),
+        ],
+      ),
 
-        ),
+      /*
+      번호 생성 버튼
       floatingActionButton: Stack (
         children: <Widget>[
-
           Align(
             alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y - 0.4),
             child: FloatingActionButton(
@@ -108,8 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-
-
+       */
 
 
 
@@ -117,141 +107,48 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget bodyContainer() {
 
+    print(selectedPos);
 
-  Widget getNumToImg2(List<int> list_lotto){
-
-    // 1번부터 10번까지는 노란색입니다.
-    // 11번 부터 20번까지는 파란색입니다.
-    // 21번부터 30번까지는 빨간색입니다.
-    // 31번부터 40번까지는 검은색입니다.
-    // 41번부터 45번까지는 초록색입니다.
-
-    Color ball_color = Colors.blue;
-    int ball_num = 0;
-
-    return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ...list_lotto.map(
-                (e) {
-                  if(e <= 10){
-                    ball_color = Colors.yellow;
-                  }else if(e > 10 && e <= 20 ){
-                    ball_color = Colors.blue;
-                  }else if(e > 20 && e <= 30 ){
-                    ball_color = Colors.red;
-                  }else if(e > 30 && e <= 40 ){
-                    ball_color = Colors.black;
-                  }else if(e > 40 && e <= 45 ){
-                    ball_color = Colors.green;
-                  }
-
-                  return SimpleShadow(
-                    opacity: 1,         // Default: 0.5
-                    color: Colors.grey,   // Default: Black
-                    offset: Offset(0, 0), // Default: Offset(2, 2)
-                    sigma: 4,             // Default: 2
-                    child: Container(
-                      width: 40,
-                      height: 40,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: ball_color,
-                      ),
-                      child: SimpleShadow(
-                        child: Text(
-                          "${e}",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        opacity: 1,         // Default: 0.5
-                        color: Colors.black,   // Default: Black
-                        offset: Offset(0, 0), // Default: Offset(2, 2)
-                        sigma: 4,             // Default: 2
-                      ),
-                    )
-                  );
-                    },
-              ),
-              Container(
-                width: 60,
-                margin: EdgeInsets.only(left: 20),
-                child: ElevatedButton(
-                  onPressed: () => getNumbers(list_lotto),
-                  child: const Text("번호 저장",
-                        style: TextStyle(fontSize: 10,),
-                        textAlign: TextAlign.center
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.deepPurple, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                ),
-              ),
-            ],
-          );
-  }
-
-  List<Widget> getNumToImg(List<int> list_lotto) {
-    List<Widget> list_lotto_img = [];
-
-    int whileIndex = 0;
-    while (whileIndex < list_lotto.length) {
-      list_lotto_img[whileIndex] = Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.black.withOpacity(0.25), // border color
-          shape: BoxShape.circle,
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(2), // border width
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.red, // inner circle color
-            ),
-            child: Container(
-                width: 70,
-                height: 70,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.green, // border color
-                  shape: BoxShape.circle,
-                ),
-                child: Container(
-                  child: Text(
-                    list_lotto[whileIndex].toString(),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                )),
-          ),
-        ),
-      );
-      whileIndex++;
+    switch (selectedPos) {
+      case 0:
+        //return const HomeScreen();
+        break;
+      case 1:
+        //return const HomeScreen();
+        break;
+      case 2:
+        //return const HomeScreen();
+        break;
+      case 3:
+        //return const HomeScreen();
+        break;
     }
 
-    return list_lotto_img;
+    return HomeScreen();
+
   }
 
-  getNumbers(List<int> list_lotto) async{
+  Widget bottomNav() {
+    return CircularBottomNavigation(
+      tabItems,
+      controller: _navigationController,
+      barHeight: bottomNavBarHeight,
+      barBackgroundColor: Colors.white,
+      animationDuration: Duration(milliseconds: 300),
+      selectedCallback: (selectedPos) {
+        setState(() {
+          this.selectedPos = selectedPos!;
+        });
+      },
+    );
+  }
 
-    print("저장버튼 눌렷드아아아아");
-    print("numbers : ${list_lotto}");
-    String strarry = list_lotto.join(",");
 
-    var insertResult = await dbHelper.insertLotto(Lotto(numbers: strarry));
-
-    List allLotto = await dbHelper.getAllLotto();
-    print(allLotto.length);
-
-    await Utils.showToast('토스트 테스트');
-
-
-
+  @override
+  void dispose() {
+    super.dispose();
+    _navigationController.dispose();
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:lotto/data/lotto.dart';
+import 'package:lotto/data/util.dart';
+import 'package:lotto/data/database.dart';
+
+import 'package:simple_shadow/simple_shadow.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreen();
+  }
+}
+
+class _HomeScreen extends State<HomeScreen> {
+
+  final dbHelper = DatabaseHelper.instance;
+
+  List<List<int>> list_lotto = [];
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Stack(
+      children: <Widget>[
+        ListView.separated(
+        padding: EdgeInsets.all(8),
+        reverse: false,
+        itemCount: list_lotto.length,
+        itemBuilder: (context, index) {
+        return getNumToImg2(list_lotto[index]);
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+       },
+    ),
+        Stack (
+          children: <Widget>[
+            Align(
+              alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y - 0.4),
+              child: FloatingActionButton(
+                onPressed: _tableDrop,
+                tooltip: '테이블 드랍',
+                child: Icon(Icons.delete),
+              ),
+            ),
+            Align(
+              alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y - 0.2),
+              child: FloatingActionButton(
+                onPressed: _addLotto,
+                tooltip: '로또번호 생성',
+                child: Icon(Icons.create),
+              ),
+            ),
+          ],
+        ),
+
+      ],
+    );
+  }
+
+  Widget getNumToImg2(List<int> list_lotto){
+
+    // 1번부터 10번까지는 노란색입니다.
+    // 11번 부터 20번까지는 파란색입니다.
+    // 21번부터 30번까지는 빨간색입니다.
+    // 31번부터 40번까지는 검은색입니다.
+    // 41번부터 45번까지는 초록색입니다.
+
+    Color ball_color = Colors.blue;
+    int ball_num = 0;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ...list_lotto.map(
+              (e) {
+            if(e <= 10){
+              ball_color = Colors.yellow;
+            }else if(e > 10 && e <= 20 ){
+              ball_color = Colors.blue;
+            }else if(e > 20 && e <= 30 ){
+              ball_color = Colors.red;
+            }else if(e > 30 && e <= 40 ){
+              ball_color = Colors.black;
+            }else if(e > 40 && e <= 45 ){
+              ball_color = Colors.green;
+            }
+
+            return SimpleShadow(
+                opacity: 1,         // Default: 0.5
+                color: Colors.grey,   // Default: Black
+                offset: Offset(0, 0), // Default: Offset(2, 2)
+                sigma: 4,             // Default: 2
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: ball_color,
+                  ),
+                  child: SimpleShadow(
+                    child: Text(
+                      "${e}",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    opacity: 1,         // Default: 0.5
+                    color: Colors.black,   // Default: Black
+                    offset: Offset(0, 0), // Default: Offset(2, 2)
+                    sigma: 4,             // Default: 2
+                  ),
+                )
+            );
+          },
+        ),
+        Container(
+          width: 60,
+          margin: EdgeInsets.only(left: 20),
+          child: ElevatedButton(
+            onPressed: () => getNumbers(list_lotto),
+            child: const Text("번호 저장",
+                style: TextStyle(fontSize: 10,),
+                textAlign: TextAlign.center
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.deepPurple, // background
+              onPrimary: Colors.white, // foreground
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  getNumbers(List<int> list_lotto) async{
+
+    print("저장버튼 눌렷드아아아아");
+    print("numbers : ${list_lotto}");
+    String strarry = list_lotto.join(",");
+
+    var insertResult = await dbHelper.insertLotto(Lotto(numbers: strarry));
+
+    List allLotto = await dbHelper.getAllLotto();
+    print(allLotto.length);
+
+    await Utils.showToast('토스트 테스트');
+  }
+
+  void _addLotto() {
+    setState(() {
+      List<int> lotto = [];
+      while (true) {
+        int rnd = Random().nextInt(45) + 1;
+        if (!lotto.contains(rnd)) {
+          lotto.add(rnd);
+        }
+        if (lotto.length == 6) break;
+      }
+
+      lotto.sort();
+      list_lotto.add(lotto);
+    });
+  }
+
+  void _tableDrop() {
+    //드랍 테이블
+    dbHelper.dropTable('getchaLotto');
+    print("드랍 테이블");
+
+    super.setState(() {
+
+      list_lotto = [];
+    });
+  }
+}
+
